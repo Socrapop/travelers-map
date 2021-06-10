bbcc91b562b306f172ab077cf179b84d2699d976
Camille V <socrapop@gmail.com>
2021-06-10 19:20:13 +0200

    [Page builders incompatibility] Return if container is not found.
    Prevent some plugins incompatibilities where the shortcode is executed multiple times, sending additionnal cttm_shortcode_... variables in the front-end.
    This way, we prevent an error where the container was not found, by stopping the script if the container is not found.

--- includes/public/js/travelersmap.js
+++ includes/public/js/travelersmap.js
@@ -24,7 +24,7 @@ function initTravelersMap() {
   //Index of map objects in array
   let mapindex = 0;
   //Create cttm_map public array of object if not already created
-  if (typeof cttm_map === "undefined") {
+  if (typeof cttm_map === 'undefined') {
     window.cttm_map = new Array();
   } else {
     //if our maps are already initialized, this removes them nicely before initializing them again. Can be used to refresh the maps.
@@ -38,9 +38,10 @@ function initTravelersMap() {
 
   function cttmMapLoop(cttm_shortcode_vars) {
     //If no markers are loaded, return without initiating leaflet
-    if(cttm_shortcode_vars.cttm_metas == "0"){
+    if (cttm_shortcode_vars.cttm_metas == '0') {
       return;
     }
+
     //Get shortcode options
     let json_cttm_shortcode = cttm_shortcode_vars.cttm_shortcode_options;
 
@@ -64,16 +65,16 @@ function initTravelersMap() {
     ];
 
     // If one-finger touch event is disabled on mobile
-    if (cttm_options["onefinger"]) {
+    if (cttm_options['onefinger']) {
       cttm_map_options.dragging = !L.Browser.mobile;
       cttm_map_options.tap = !L.Browser.mobile;
     }
     //Set maxzoom if defined
-    if (cttm_shortcode_options.maxzoom != "") {
+    if (cttm_shortcode_options.maxzoom != '') {
       cttm_map_options.maxZoom = cttm_shortcode_options.maxzoom;
     }
     //Set minzoom if defined
-    if (cttm_shortcode_options.minzoom != "") {
+    if (cttm_shortcode_options.minzoom != '') {
       cttm_map_options.minZoom = cttm_shortcode_options.minzoom;
     }
     //Set init_maxzoom if defined. Convert to integer to avoid error.
@@ -98,26 +99,34 @@ function initTravelersMap() {
     //Get cttm_map container id
     let containerid = cttm_shortcode_options.id;
     let container = document.getElementById(
-      "travelersmap-container-" + containerid
+      'travelersmap-container-' + containerid
     );
+    //Prevent some plugins incompatibilities where the shortcode is executed multiple times, sending additionnal cttm_shortcode_... variables in the front-end.
+    // This way, we prevent an error where the container was not found, by stopping the script if the container is not found.
+    if (!container) {
+      console.warn(
+        "Travelers' Map container with id: " + containerid + ' was not found.'
+      );
+      return;
+    }
 
     //Set Tiles Server URL + API key + Attribution
     //If a shortcode tile server is set, override global settings' tile server
     let tileurl, subdomains, attribution;
-    if (cttm_shortcode_options.tileurl !== "") {
+    if (cttm_shortcode_options.tileurl !== '') {
       tileurl = cttm_shortcode_options.tileurl;
     } else {
-      tileurl = cttm_options["tileurl"];
+      tileurl = cttm_options['tileurl'];
     }
-    if (cttm_shortcode_options.subdomains !== "") {
+    if (cttm_shortcode_options.subdomains !== '') {
       subdomains = cttm_shortcode_options.subdomains;
     } else {
-      subdomains = cttm_options["subdomains"];
+      subdomains = cttm_options['subdomains'];
     }
-    if (cttm_shortcode_options.attribution !== "") {
+    if (cttm_shortcode_options.attribution !== '') {
       attribution = cttm_shortcode_options.attribution;
     } else {
-      attribution = cttm_options["attribution"];
+      attribution = cttm_options['attribution'];
     }
 
     //Push current map object to array
@@ -128,13 +137,12 @@ function initTravelersMap() {
       attribution: attribution,
     }).addTo(cttm_map[mapindex]);
 
-
     /**
      * Disable Scrollwheel zoom when map is not in focus
      */
     cttm_map[mapindex].scrollWheelZoom.disable();
     //Enable Scrollwheel Zoom on focus
-    cttm_map[mapindex].on("focus", function (e) {
+    cttm_map[mapindex].on('focus', function (e) {
       this.scrollWheelZoom.enable();
     });
 
@@ -151,7 +159,7 @@ function initTravelersMap() {
     //Create a markerClusterGroup if Clustering is activated (default),
     //else, create a FeatureGroup instead (we need "getBounds()" method for initial zoom, that's why we don't use a LayerGroup.)
     let markersGroup;
-    if (cttm_shortcode_options.disable_clustering === "true") {
+    if (cttm_shortcode_options.disable_clustering === 'true') {
       markersGroup = L.featureGroup();
     } else {
       //default
@@ -160,9 +168,9 @@ function initTravelersMap() {
         maxClusterRadius: max_cluster_radius,
         spiderLegPolylineOptions: {
           weight: 3,
-          color: "rgb(110, 204, 57)",
+          color: 'rgb(110, 204, 57)',
           opacity: 0.6,
-          lineCap: "round",
+          lineCap: 'round',
         },
         spiderfyDistanceMultiplier: 2.5,
       });
@@ -220,7 +228,7 @@ function initTravelersMap() {
 
           // Create a leaflet icon object and add it to the map, if not set, use default
           //"d" is returned when no icon is set
-          if (markerURL != "d") {
+          if (markerURL != 'd') {
             //Create custom icon
             let myIcon = L.icon({
               iconUrl: markerURL,
@@ -246,7 +254,7 @@ function initTravelersMap() {
           //If "this_post" option is set
           //Add the marker in our cluster group layer without popover
           //Else add it with its popover
-          if (cttm_shortcode_options.this_post == "true") {
+          if (cttm_shortcode_options.this_post == 'true') {
             markersGroup.addLayer(marker);
           } else {
             markersGroup.addLayer(
@@ -257,14 +265,13 @@ function initTravelersMap() {
       } //END For Loop through cttm_metas
 
       //add Leaflet.search to the map when option is checked
-      if (cttm_options["search_field"] == 1) {
+      if (cttm_options['search_field'] == 1) {
         cttm_map[mapindex].addControl(
           new L.Control.Search({
-            url:
-              "https://nominatim.openstreetmap.org/search?format=json&q={s}",
-            jsonpParam: "json_callback",
-            propertyName: "display_name",
-            propertyLoc: ["lat", "lon"],
+            url: 'https://nominatim.openstreetmap.org/search?format=json&q={s}',
+            jsonpParam: 'json_callback',
+            propertyName: 'display_name',
+            propertyLoc: ['lat', 'lon'],
             autoCollapse: false,
             collapsed: false,
             autoType: true,
@@ -276,8 +283,8 @@ function initTravelersMap() {
         );
 
         //On focus, enable zoom with mousewheel on map.
-        document.querySelector("#searchtext9").addEventListener(
-          "focus",
+        document.querySelector('#searchtext9').addEventListener(
+          'focus',
           function () {
             cttm_map[mapindex].scrollWheelZoom.enable();
           },
@@ -286,10 +293,10 @@ function initTravelersMap() {
       }
 
       //add Leaflet.fullscreen to the map when option is checked
-      if (cttm_options["fullscreen_button"] == 1) {
+      if (cttm_options['fullscreen_button'] == 1) {
         cttm_map[mapindex].addControl(
           new L.Control.Fullscreen({
-            position: "topright",
+            position: 'topright',
           })
         );
       }
@@ -299,7 +306,7 @@ function initTravelersMap() {
 
       //Set the initial view
       //If centered_on_this is set, set view on this post
-      if (cttm_shortcode_options.centered_on_this == "true") {
+      if (cttm_shortcode_options.centered_on_this == 'true') {
         //get the marker latitude and longitude, the first of our query.
         let centered_on_marker = JSON.parse(cttm_metas[0].markerdatas);
         let centerlatitude = centered_on_marker.latitude;
@@ -316,7 +323,6 @@ function initTravelersMap() {
           maxZoom: init_maxzoom,
         });
       }
-
     } //END if (!json_cttm_metas)
 
     //Recalculate map size after 100ms to avoid problems with page builders changing element size on document load.
@@ -324,7 +330,7 @@ function initTravelersMap() {
 
     const mapindexcopy = mapindex;
     setTimeout(() => {
-      cttm_map[mapindexcopy].invalidateSize()
+      cttm_map[mapindexcopy].invalidateSize();
     }, 1000);
 
     mapindex++;
@@ -332,17 +338,17 @@ function initTravelersMap() {
 
   // Create event to listen to know when the maps are loaded and cttm_map array is created
   // Useful if you want to add a leaflet plugin to your maps.
-  let event_cttm = document.createEvent("Event");
+  let event_cttm = document.createEvent('Event');
 
   // Define that the event name is 'build'.
-  event_cttm.initEvent("cttm_map_loaded", true, true);
+  event_cttm.initEvent('cttm_map_loaded', true, true);
 
   // target can be any Element or other EventTarget.
   document.dispatchEvent(event_cttm);
 } //end function InitTravelersMap
 
-document.addEventListener("DOMContentLoaded", function () {
-  if (document.getElementsByClassName("travelersmap-container")) {
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.getElementsByClassName('travelersmap-container')) {
     initTravelersMap();
   }
 });
@@ -360,18 +366,18 @@ function cttmGeneratePopoverHTMLOutput(cttm_shortcode_options, cttm_options) {
   let popoverTarget;
   let popoverOutput;
   let popoverOptions;
-  if (cttm_shortcode_options.open_link_in_new_tab === "true") {
-    popoverTarget = "_blank";
+  if (cttm_shortcode_options.open_link_in_new_tab === 'true') {
+    popoverTarget = '_blank';
   } else {
-    popoverTarget = "_self";
+    popoverTarget = '_self';
   }
   //Then we create HMTL output for popovers depending of style set in plugin settings
-  let popoverStyles = cttm_options["popup_style"].split(",");
+  let popoverStyles = cttm_options['popup_style'].split(',');
 
-  if (popoverStyles.indexOf("thumbnail") != -1) {
-    if (popoverStyles.indexOf("excerpt") != -1) {
+  if (popoverStyles.indexOf('thumbnail') != -1) {
+    if (popoverStyles.indexOf('excerpt') != -1) {
       //Detailed Popup : Thumbnail and excerpt, with (title) and (date). () = optionnal
-      popoverOptions = { className: "detailed-popup" };
+      popoverOptions = { className: 'detailed-popup' };
       popoverOutput =
         '<a class="tooltip-link" href="%s_url" target="' + popoverTarget + '">';
       popoverOutput += '<div class="nothumbplaceholder"></div>';
@@ -380,7 +386,7 @@ function cttmGeneratePopoverHTMLOutput(cttm_shortcode_options, cttm_options) {
       popoverOutput += '<div class="excerpt">%s_excerpt</div>';
     } else {
       //Default Popup : Thumbnail with (title) and (date). () = optionnal
-      popoverOptions = { className: "default-popup" };
+      popoverOptions = { className: 'default-popup' };
 
       popoverOutput = '<div class="img-mask">';
       popoverOutput += '<div class="nothumbplaceholder"></div>';
@@ -391,11 +397,11 @@ function cttmGeneratePopoverHTMLOutput(cttm_shortcode_options, cttm_options) {
       popoverOutput += '<div class="popup-thumb-text-wrapper">';
       popoverOutput += '<div class="title">%s_title</div>';
       popoverOutput += '<div class="date">%s_date</div>';
-      popoverOutput += "</div></a>";
+      popoverOutput += '</div></a>';
     }
   } else {
     //Textual Popup : excerpt, title and date. At least one or more of those.
-    popoverOptions = { className: "textual-popup" };
+    popoverOptions = { className: 'textual-popup' };
 
     popoverOutput =
       '<a class="tooltip-link" href="%s_url" target="' + popoverTarget + '">';
@@ -405,8 +411,8 @@ function cttmGeneratePopoverHTMLOutput(cttm_shortcode_options, cttm_options) {
   }
 
   //If css is disabled, change popover class
-  if (cttm_options["popup_css"]) {
-    popoverOptions = { className: "custom-popup" };
+  if (cttm_options['popup_css']) {
+    popoverOptions = { className: 'custom-popup' };
   }
   return [popoverOutput, popoverOptions];
 }
@@ -429,11 +435,11 @@ function cttmPopulatePopoversHTMLOutput(
   let postExcerpt = postdatas.excerpt;
   let postDate = new Date(postdatas.date);
   postDate = postDate.toLocaleDateString(undefined, {
-    year: "numeric",
-    month: "long",
-    day: "numeric",
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
   });
-  let popoverStyles = cttm_options["popup_style"].split(",");
+  let popoverStyles = cttm_options['popup_style'].split(',');
   let postPopoverOutput = popoverOutput;
 
   if (postThumb) {
@@ -442,31 +448,31 @@ function cttmPopulatePopoversHTMLOutput(
       '<img src="' + postThumb + '" alt="">'
     );
   }
-  if (postExcerpt && popoverStyles.indexOf("excerpt") != -1) {
-    postPopoverOutput = postPopoverOutput.replace("%s_excerpt", postExcerpt);
+  if (postExcerpt && popoverStyles.indexOf('excerpt') != -1) {
+    postPopoverOutput = postPopoverOutput.replace('%s_excerpt', postExcerpt);
   } else {
     postPopoverOutput = postPopoverOutput.replace(
       '<div class="excerpt">%s_excerpt</div>',
-      ""
+      ''
     );
   }
-  if (postTitle && popoverStyles.indexOf("title") != -1) {
-    postPopoverOutput = postPopoverOutput.replace("%s_title", postTitle);
+  if (postTitle && popoverStyles.indexOf('title') != -1) {
+    postPopoverOutput = postPopoverOutput.replace('%s_title', postTitle);
   } else {
     postPopoverOutput = postPopoverOutput.replace(
       '<div class="title">%s_title</div>',
-      ""
+      ''
     );
   }
-  if (postDate && popoverStyles.indexOf("date") != -1) {
-    postPopoverOutput = postPopoverOutput.replace("%s_date", postDate);
+  if (postDate && popoverStyles.indexOf('date') != -1) {
+    postPopoverOutput = postPopoverOutput.replace('%s_date', postDate);
   } else {
     postPopoverOutput = postPopoverOutput.replace(
       '<div class="date">%s_date</div>',
-      ""
+      ''
     );
   }
-  postPopoverOutput = postPopoverOutput.replace("%s_url", posturl);
+  postPopoverOutput = postPopoverOutput.replace('%s_url', posturl);
 
   return postPopoverOutput;
 }
