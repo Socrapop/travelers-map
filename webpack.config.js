12c26cfaeb61ef960092f6d2daff53f142cf1a66
Camille V <socrapop@gmail.com>
2022-02-25 19:46:34 +0100

    Removed IIFE inserted around global functions of Travelers Map
    initTravelersMap() was not working anymore to asynchronously load the maps.
    
    Issue : https://github.com/Socrapop/travelers-map/issues/26

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
   output: {
     filename: 'travelersmap-bundle.js',
     path: path.resolve(__dirname, './includes/public/js/dist'),
+    iife: false,
   },
   optimization: {
     minimize: false,
